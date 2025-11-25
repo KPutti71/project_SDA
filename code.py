1ca09b81ce8beee9af86ff97ab17ff322e4590f0
Kyrian Puttileihalat <kputti@Kyrians-Laptop.local>
2025-11-25 12:25:30 +0100

    plot bitcoin and gold

--- code.py
+++ code.py
@@ -11,13 +11,33 @@ import matplotlib.pyplot as plt
 Read csv files
 """
 
-bitcoin = pd.read_csv("project_SDA/datasets/Bitcoin.csv", sep=',')
+bitcoin = pd.read_csv("datasets/bitcoin_prices.csv", sep=',')
+gold = pd.read_csv("datasets/gold_prices.csv", sep=',')
 
-bitcoin_open = bitcoin[['Date', 'Open']]
+ #%%
+"""
+Test plot for bitcoin and gold open prices over time
+"""
 
-open_prices = bitcoin_open['Open']
-time = np.arange(len(open_prices))
+bitcoin_open = bitcoin['Open']
+bitcoin_open_time = np.arange(len(bitcoin_open))
 
-plt.plot(time, open_prices)
+gold_open = gold['Open']
+gold_open_time = np.arange(len(gold_open))
+
+plt.figure(figsize=(11,5))
+
+plt.subplot(1,2,1)
+plt.plot(bitcoin_open_time, bitcoin_open)
+plt.title('Bitcoin price over time')
+
+plt.subplot(1,2,2)
+plt.plot(gold_open_time, gold_open)
+plt.title('Gold price over time')
+
 plt.show()
 
+# %%
+"""
+
+"""
