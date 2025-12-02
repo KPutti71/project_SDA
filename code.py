2d518e1e532f5c8024f20f15354010d6e257a9dc
Kyrian Puttileihalat <kputti@Kyrians-Laptop.local>
2025-12-02 15:43:58 +0100

    handled data, improved LRM for generality

--- code.py
+++ code.py
@@ -14,62 +14,68 @@ Read csv files
 bitcoin = pd.read_csv("datasets/bitcoin_prices.csv", sep=',')
 gold = pd.read_csv("datasets/gold_prices.csv", sep=',')
 
+# %%
+"""
+Reading new file containing all stocks/btc/gold prices and volumes
+"""
+
 full_file = pd.read_csv("datasets/djia30_btc_gold.csv", sep=',')
 
-# Remove the weekends since stock market is closed so we wont have any values
+# Remove the rows with NAN values since stock market is closed so we wont have any values
 df = full_file.dropna()
-print(df['AMZN'])
+
+# Remove row with indication of stock price or volume since we can identify with column names
+df_transpose = df.T
+df_transpose.pop(0)
+df = df_transpose.T
 
  #%%
 """
-Test plot for bitcoin and gold open prices over time
+Test plot for AMZN closing prices over time
 """
 
-bitcoin_open = bitcoin['Open']
-bitcoin_open_time = np.arange(len(bitcoin_open))
+# Get closing prices for AMZN and calculate the evaluated time
+AMZN_cls = df["AMZN"][1:].astype('float')
+AMZN_cls_time = np.arange(len(AMZN_cls))
 
-gold_open = gold['Open']
-gold_open_time = np.arange(len(gold_open))
-
-# plt.figure(figsize=(11,5))
-
-# plt.subplot(1,2,1)
-# plt.plot(bitcoin_open_time, bitcoin_open)
-# plt.title('Bitcoin price over time')
-
-# plt.subplot(1,2,2)
-# plt.plot(gold_open_time, gold_open)
-# plt.title('Gold price over time')
-
-# plt.show()
+# Plot a test figure for one example
+plt.figure(figsize=(11,5))
+plt.plot(AMZN_cls_time, AMZN_cls)
+plt.title('AMZN closing price over time')
+plt.show()
 
 # %%
 """
-Fitting LRM for n features and m observations
-Y = beta * Xf
+Fitting LRM with a minimizing RSS method
+Y = beta * X
 """
 
-n = 1
-m = len(gold_open_time)
-x1 = gold_open_time
+# n is the number of features and m is the dimension of the given features
+# for all items in x we assume that they have the same dimension which me made sure of in the removing NAN values in df
+x = [AMZN_cls_time]
+n = len(x)
+m = len(x[0])
+Y = AMZN_cls
 
-# C olumn one should be ones for the intercept
+# Column one should be ones for the intercept
 X = np.zeros((m,n+1))
 X[:,0] = 1
+print(X)
 
-# Loop over columns and rows to fill in obeserved data x
+# Loop over columns to fill in obeserved data x
 for col in range(n):
-    for row in range(m):
-        X[row, col+1] = x1[row]
+    X[:, col+1] = x[col]
+print(X)
 
 # Calculate the parameter 
-Y = gold_open
 beta = np.linalg.inv(X.T @ X) @ X.T @ Y
+print(beta)
 
 # Make plot of the fit
-plt.plot(gold_open_time, gold_open)
-plt.plot(gold_open_time, beta[1] * gold_open_time + beta[0])
-plt.title('Gold price over time')
+plt.figure(figsize=(11,5))
+plt.plot(AMZN_cls_time, AMZN_cls)
+plt.plot(AMZN_cls_time,  + beta[0] + beta[1] * x[0])
+plt.title('AMZN closing price over time with fitted LRM')
 plt.show()
 
 # %%
